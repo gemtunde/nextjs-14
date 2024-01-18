2a01fd30f3b58eb585d03ff84f56c5819a6f2f5d
gemtunde <gemtunde@gmail.com>
2024-01-18 17:18:42 +0100

    route groups

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const ForgotPassword = (props: Props) => {
+  return (
+    <div>
+      ForgotPassword - used to create developer experience. nextjs ommit the
+      (auth) name in the browser routing
+    </div>
+  );
+};
+
+export default ForgotPassword;
