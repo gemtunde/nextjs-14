608900963523567f1a40620288d0a56d795e157f
gemtunde <gemtunde@gmail.com>
2024-01-18 10:09:27 +0100

    nested route

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function First({}: Props) {
+  return <div>First</div>;
+}
+
+export default First;
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Second = (props: Props) => {
+  return <div>Second</div>;
+};
+
+export default Second;
