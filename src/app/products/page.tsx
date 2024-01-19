01386fdc885ae69adc5103cd48e5cda930a527a1
gemtunde <gemtunde@gmail.com>
2024-01-19 09:03:36 +0100

    links

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
@@ -8,7 +9,21 @@ export const metadata = {
 };
 
 const ProductDetails = (props: Props) => {
-  return <div>ProductDetails</div>;
+  return (
+    <div>
+      Product Details
+      <h2>
+        <Link href="products/1"> Product 1 </Link>
+      </h2>
+      <h2>
+        {" "}
+        <Link href="products/2"> Product 2 </Link>
+      </h2>
+      <h2>
+        <Link href="products/3"> Product 3 </Link>
+      </h2>
+    </div>
+  );
 };
 
 export default ProductDetails;
