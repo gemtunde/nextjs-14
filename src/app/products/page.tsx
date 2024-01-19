d5bd6c113a0d138393d95b4af7ce01230b6e8b16
gemtunde <gemtunde@gmail.com>
2024-01-19 05:05:01 +0100

    static meta data

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 type Props = {};
+export const metadata = {
+  title: "About Products Page",
+};
 
 const ProductDetails = (props: Props) => {
   return <div>ProductDetails</div>;
